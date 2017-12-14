2fdd41dcbedfe6e82f54047ac72e770565599b1f
KingJA <kingjavip@gmail.com>
2017-12-14 23:57:08 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <component-a></component-a>
+    <component-a event="hi,boy" v-on:child-said="sonSaid"></component-a>
+    <h3>son said:{{sonWords}}</h3>
     <h1>{{msg}}</h1>
     <span><input type="text" v-model="newTodo" v-on:keyup.enter="addTodo()"><button
       @click="addTodo()">添加</button></span>
@@ -14,28 +15,42 @@
 <script>
   import Store from './store';
   import ComponentA from './components/ComponentA';
-
+  import {myEventBus} from './event-bus';
 
   export default {
     data() {
       return {
         msg: 'this is a todo list.',
         newTodo: '',
+        sonWords: '',
         todos: Store.fetch()
       }
     },
     components: {ComponentA},
+    created: function () {
+      // myEventBus.$on('child-said', function (msg) {
+      //   console.log("msg:"+msg);
+      //   this.sonWords = msg;
+      // })
+    }
+    ,
     methods: {
       addTodo: function () {
         this.todos.push({action: this.newTodo, finished: false})
         this.newTodo = '';
+      },
+      sonSaid:function (sonMsg) {
+        console.log("sonMsg:"+sonMsg);
+        this.sonWords = sonMsg;
       }
-    },
+    }
+    ,
     watch: {
       todos: {
         handler: function (items) {
           Store.save(items)
-        },
+        }
+        ,
         deep: true
       }
     }
--- src/components/ComponentA.vue
+++ src/components/ComponentA.vue
@@ -1,19 +1,35 @@
 <template>
-    <h1>{{msg}}</h1>
+  <div>
+    <h1>Dad said:{{msg}}</h1>
+    <button @click="receiver()">receive from father</button>
+    <button @click="sender()">send to father</button>
+  </div>
 </template>
 
 <script>
-    export default {
-       data() {
-         return{
-           msg:'Hello ,CommonentA'
-         }
-       }
+  export default {
+    data() {
+      return {
+        msg: '',
+        sendMsg: 'hello, fahter'
+      }
+    },
+    props: ['event'],
+    methods: {
+      receiver: function () {
+        this.msg = this.event;
+      },
+      sender: function () {
+        console.log("sender")
+        this.$emit('child-said', this.sendMsg);
+      }
     }
+  }
 </script>
 
 <style scoped>
-  h1{
-    color: #5896ff;}
+  h1 {
+    color: #5896ff;
+  }
 
 </style>
